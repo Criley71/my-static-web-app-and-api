0e78f20b164e5121d6e574dfeb9462b73f2d464d
connor <connorriley@outlook.com>
2023-10-07 18:18:39 -0400

    added get function

---
+++ api/products-get/index.js
@@ -0,0 +1,13 @@
+const data = require('../shared/product-data')
+
+module.exports = async function (context, req) {
+    try{
+        const product = data.getProducts();
+        context.res.status(200).json(products);
+        console.log(200);
+    }
+    catch(err) {
+        context.res.status(500).send(err);
+        console.log(500);
+    }
+}; 
